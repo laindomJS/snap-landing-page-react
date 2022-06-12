462bf84a329446e25a1e96d8fd798a36ee20abd7
Adrian Rangel <adrijar21@gmail.com>
2022-06-12 18:53:17 -0400

    index css deleted

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import App from './App'
-import './index.css'
 
 const colors = {
   almostWhite: {
